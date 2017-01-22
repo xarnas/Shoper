8437af5fac34f080081bbc4cb8cf85af5218de9a
xarnas <arnpetrauskas@gmail.com>
2017-01-22 22:45:33 +0200

    App 1 pic done
    app 1 pic done

---
+++ app/src/main/java/com/arnas/shoper/MainActivity.java
@@ -0,0 +1,118 @@
+
+package com.arnas.shoper;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView ;
+    String[] values= new String[1];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        LinearLayout ll = new LinearLayout(this);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+               // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //   .setAction("Action", null).show()
+
+               // values[0]="Ramune";
+                // listView.invalidateViews();
+
+
+                setContentView(R.layout.singleitem);
+
+                Button addCheckBox = (Button)findViewById(R.id.addCheckBox);
+
+              //  LinearLayout ll = new LinearLayout(this);
+
+
+                addCheckBox.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        for(int i = 0; i < 20; i++) {
+                            CheckBox cb = new CheckBox(getApplicationContext());
+                            cb.setText("I'm dynamic!");
+                            ll.addView(cb);
+
+                        }
+                    }
+                });
+
+
+            }
+        });
+
+        values[0]="Arnas";
+        this.listView = (ListView)findViewById(R.id.list);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, android.R.id.text1, this.values);
+
+            this.listView.setAdapter(adapter);
+
+            this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+
+                    int itemPosition = position;
+                    String itemValue = (String) listView.getItemAtPosition(position);
+                    Toast.makeText(getApplicationContext(),
+                            "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                            .show();
+
+                }
+
+            });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
+
